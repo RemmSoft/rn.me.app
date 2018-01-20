798ea344180147e4bd99b4561485b3c37c8f1b6a
mahmut akyol <mahmut.akyol@outlook.com.tr>
2018-01-20 14:50:44 +0300

    Password alanı tasarımı düzenlendi.

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { ImageBackground, View, StatusBar } from "react-native";
+import { ImageBackground, View, StatusBar, KeyboardAvoidingView } from "react-native";
 import {
   Container,Button,H3,Text,Header,Title,Content,Item,Tab,Tabs,
   TabHeading,Label,Input,Body,Left,Right,Icon,Form,Card,CardItem
@@ -18,74 +18,74 @@ const tabProps = {
 class Home extends Component {
   render() {
     return (
-      <Container>
-        <StatusBar barStyle="light-content" />
-        <ImageBackground source={launchscreenBg} style={styles.imageContainer}>
-          <View style={styles.logoContainer}>
+        <Container>
+          <StatusBar barStyle="light-content" />
+          <ImageBackground source={launchscreenBg} style={styles.imageContainer}>
+            <View style={styles.logoContainer}>
 
-          </View>
-          <Tabs style={{ elevation: 2}} {...tabProps}>
-          <Tab
-            heading={
-              <TabHeading>
-                <Text>Login</Text>
-              </TabHeading>
-            }
-          >
-          <Content>
-          <Form>
-            <Item floatingLabel>
-              <Label>Email</Label>
-              <Input style={styles.colorWhite}/>
-            </Item>
-            <Item floatingLabel last>
-              <Label>Password</Label>
-              <Input secureTextEntry style={styles.colorWhite}/>
-            </Item>
-          </Form>
-          <Button block style={styles.button}>
-            <Text>Sign In</Text>
-          </Button>
-        </Content>
-          </Tab>
-          <Tab
-            heading={
-              <TabHeading>
-                <Text>Register</Text>
-              </TabHeading>
-            }
-          >
-           <Content>
-          <Form>
-          <Item floatingLabel>
-              <Label>Name</Label>
-              <Input style={styles.colorWhite}/>
-            </Item>
-            <Item floatingLabel>
-              <Label>Email</Label>
-              <Input style={styles.colorWhite}/>
-            </Item>
-            <Item floatingLabel last>
-              <Label>Password</Label>
-              <Input secureTextEntry style={styles.colorWhite}/>
-            </Item>
-            <Item floatingLabel>
-              <Label>Phone</Label>
-              <Input style={styles.colorWhite}/>
-            </Item>
-            <Item floatingLabel>
-              <Label>UserType</Label>
-              <Input style={styles.colorWhite}/>
-            </Item>
-          </Form>
-          <Button block style={styles.button}>
-            <Text>Sign Up</Text>
-          </Button>
-        </Content>
-          </Tab>
-        </Tabs>
+            </View>
+            <Tabs style={{ elevation: 2}} {...tabProps}>
+              <Tab
+                heading={
+                  <TabHeading>
+                    <Text>Login</Text>
+                  </TabHeading>
+                }
+              >
+                <Content>
+                  <Form>
+                    <Item floatingLabel>
+                      <Label>Email</Label>
+                      <Input style={styles.colorWhite}/>
+                    </Item>
+                    <Item floatingLabel>
+                      <Label>Password</Label>
+                      <Input secureTextEntry={true} style={styles.colorWhite}/>
+                    </Item>
+                  </Form>
+                  <Button block style={styles.button}>
+                    <Text>Sign In</Text>
+                  </Button>
+                </Content>
+              </Tab>
+              <Tab
+                heading={
+                  <TabHeading>
+                    <Text>Register</Text>
+                  </TabHeading>
+                }
+              >
+                <Content>
+                  <Form>
+                  <Item floatingLabel>
+                      <Label>Name</Label>
+                      <Input style={styles.colorWhite}/>
+                    </Item>
+                    <Item floatingLabel>
+                      <Label>Email</Label>
+                      <Input style={styles.colorWhite}/>
+                    </Item>
+                    <Item floatingLabel>
+                      <Label>Password</Label>
+                      <Input secureTextEntry style={styles.colorWhite}/>
+                    </Item>
+                    <Item floatingLabel>
+                      <Label>Phone</Label>
+                      <Input style={styles.colorWhite}/>
+                    </Item>
+                    <Item floatingLabel>
+                      <Label>UserType</Label>
+                      <Input style={styles.colorWhite}/>
+                    </Item>
+                  </Form>
+                  <Button block style={styles.button}>
+                    <Text>Sign Up</Text>
+                  </Button>
+                </Content>
+              </Tab>
+            </Tabs>
         </ImageBackground>
-      </Container>
+        </Container>
     );
   }
 }
